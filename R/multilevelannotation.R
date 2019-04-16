3c56ba0522d9529e2f56b98df12a8209cb132948
MRJ <M-R-JONES@users.noreply.github.com>
2019-04-16 20:09:45 +0200

    fix customDB call

--- R/multilevelannotation.R
+++ R/multilevelannotation.R
@@ -113,7 +113,7 @@
 multilevelannotation <- function(dataA, max.mz.diff = 10, 
     max.rt.diff = 10, clustmethod = 'WGCNA', cormethod = "pearson", num_nodes = 2, 
     queryadductlist = c("all"), gradienttype = "Acetonitrile", 
-    mode = "pos", outloc, db_name = "HMDB", adduct_weights = NA, 
+    mode = "pos", outloc, db_name = "HMDB", customDB_path = '', adduct_weights = NA, 
     num_sets = 3000, allsteps = TRUE, corthresh = 0.7, NOPS_check = TRUE, 
     customIDs = NA, missing.value = NA, missing.permute = 100, deepsplit = 2, networktype = "unsigned", 
     minclustsize = 10, module.merge.dissimilarity = 0.2, 
@@ -629,7 +629,11 @@ multilevelannotation <- function(dataA, max.mz.diff = 10,
     } else if (db_name == "Custom") {
       
       #read in suspect screening database
-      customDB = customDB
+      if(customDB_path != ''){
+        customDB = read.csv(customDB_path, header = T, stringsAsFactors = F)
+      }else{
+        data(customDB)
+      }
       
       colnames(customDB)[which(colnames(customDB)=='ID')] = 'CompoundID'
       colnames(customDB)[which(colnames(customDB)=='Name')] = 'Name'
